49fa4cb1eacae0470c88cd94a022ad68b985d2bf
jacovzap <jacovza@gmail.com>
2023-06-10 14:04:40 -0400

    first commit

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,13 @@
+
+
+const Navbar = () => {
+    return (
+
+     <div className="text-3xl font-bold underline">
+        Hola a todos
+     </div>
+
+    )
+}
+
+export default Navbar
---
+++ src/layouts/MainLayout.tsx
@@ -0,0 +1,18 @@
+import Navbar from '../components/navbar/Navbar'
+
+
+interface MainLayoutProps {
+    children: React.ReactNode
+}
+
+const MainLayout = ({ children }: MainLayoutProps) => {
+    return (
+        < >
+            <Navbar />
+        </>
+
+
+    )
+}
+
+export default MainLayout
