e63aeaa98c1d9f662b050635bcb84bd2f2a31fdd
jacovzap <jacovza@gmail.com>
2023-07-07 11:28:24 -0400

    Versioning untracked files

--- src/models/DepositOrder.ts
+++ src/models/DepositOrder.ts
@@ -1,21 +1,20 @@
-import { EmployeeInterface } from "./Employee"
-import { RegionalOfficeInterface } from "./RegionalOffice"
+import { EmployeeInterface } from './Employee'
+import { RegionalOfficeInterface } from './RegionalOffice'
 
 export interface DepositOrderInterface {
-    amount: number
-    deliveryDate: Date
-    employeeId?: number
-    employee?: EmployeeInterface
-    endDate: Date
-    id?: number
-    orderNumber: string
-    regionalId?: number
-    regional?: RegionalOfficeInterface
-    revitionStatus?: string
-    solitudeDate: Date
-    startDate: Date
-    status?: string
-    documentUrl?: string
-    [key: string]: any
-  }
-  
+  amount: number
+  deliveryDate: Date
+  employeeId?: number
+  employee?: EmployeeInterface
+  endDate: Date
+  id?: number
+  orderNumber: string
+  regionalId?: number
+  regional?: RegionalOfficeInterface
+  revitionStatus?: string
+  solitudeDate: Date
+  startDate: Date
+  status?: string
+  documentUrl?: string
+  [key: string]: any
+}
